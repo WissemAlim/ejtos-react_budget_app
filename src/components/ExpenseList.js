d4c03128e5a99357388102ff840da1bea9828ad0
WissemAlim <wissemalim93@gmail.com>
2025-05-04 12:53:08 +0200

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+const ExpenseList = ({ expenses, onIncrease, onDelete }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Department</th>
+        <th>Allocated Budget</th>
+        <th>Increase by 10</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {expenses.map((item) => (
+        <ExpenseItem
+          key={item.department}
+          department={item.department}
+          cost={item.cost}
+          onIncrease={() => onIncrease(item.department)}
+          onDelete={() => onDelete(item.department)}
+        />
+      ))}
+    </tbody>
+  </table>
+);
+
+export default ExpenseList;
 
