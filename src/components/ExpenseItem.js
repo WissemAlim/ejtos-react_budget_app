8e1394b8f0a741109f6533f07963746567d61674
WissemAlim <wissemalim93@gmail.com>
2025-05-04 12:51:49 +0200

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const ExpenseItem = ({ department, cost, onIncrease, onDelete }) => (
+  <tr>
+    <td>{department}</td>
+    <td>£{cost}</td>
+    <td><button onClick={onIncrease}>+</button></td>
+    <td><button onClick={onDelete}>✖</button></td>
+  </tr>
+);
+
+export default ExpenseItem;
