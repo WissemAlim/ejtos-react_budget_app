131b2e4f8ed4b2feb27066149e3cec92b1bcc046
WissemAlim <wissemalim93@gmail.com>
2025-05-04 12:54:14 +0200

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const ExpenseTotal = ({ expenses }) => {
+  const total = expenses.reduce((sum, item) => sum + item.cost, 0);
+  return (
+    <div style={{ backgroundColor: '#cfe2ff' }}>
+      Spent so far: £{total}
+    </div>
+  );
+};
+
+export default ExpenseTotal;
