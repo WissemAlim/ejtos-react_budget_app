c6bba7a9c245915c5985a3b3f7d50e9510ab0651
WissemAlim <wissemalim93@gmail.com>
2025-05-04 12:50:41 +0200

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const Budget = ({ budget, setBudget }) => (
+  <div>
+    <label>Budget: £</label>
+    <input
+      type="number"
+      value={budget}
+      onChange={(e) => setBudget(Number(e.target.value))}
+    />
+  </div>
+);
+
+export default Budget;
